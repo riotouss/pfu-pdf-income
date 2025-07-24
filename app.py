51ac41848fc04c647db9492e7caabc9517c94302
Anastasiia <crimsonlumos@gmail.com>
2025-07-24 20:27:21 +0300

    1.0

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pdfplumber
+import re
+
+st.set_page_config(page_title="📄 Розрахунок доходу з PDF")
+
+st.title("📄 Розрахунок доходу з довідки ПФУ")
+uploaded_file = st.file_uploader("Завантаж PDF-довідку", type="pdf")
+
+if uploaded_file is not None:
+    with open("uploaded_file.pdf", "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    tmp_path = "uploaded_file.pdf"
+
+    with pdfplumber.open(tmp_path) as pdf:
+        text = "".join([page.extract_text() for page in pdf.pages])
+
+    text_fixed = re.sub(r"(?<=[а-яіїєґ0-9])(?=[А-ЯІЇЄҐ])", " ", text)
+
+    years = re.findall(r"Звітнийрік[: ]?(\d{4})", text_fixed)
+    amounts = re.findall(r"Усьогозарік[: ]?([\d\.]+)грн", text_fixed)
+    matches = list(zip(years, amounts))
+
+    if matches:
+        rows = [("Рік", "Сума", "7%", "Після вирахування")]
+        for year, amount in matches:
+            total = float(amount)
+            percent_7 = round(total * 0.07, 2)
+            after = round(total - percent_7, 2)
+            rows.append((year, total, percent_7, after))
+
+        st.success("✅ Дані оброблено:")
+        st.table(rows)
+    else:
+        st.warning("❗ Не знайдено сум за рік у файлі.")
+
